3d3e7a18d8ca782fa1360caa5f953d4aaf2f0f83
sivv1812 <sivv1218@gmail.com>
2025-09-09 23:35:37 -0400

    chems

--- Laboratorios/laboratorio_1/main.py
+++
@@ -1,6 +0,0 @@
-from animales import animales
-
-# Se elimino la linea if __name__ == '__main__': debido a se un if que no tiene sentido si estar dentro de una función.
-print("Ingrese el animalito para saber que dice:")
-animal = input()
-animales(animal)
